30efe87281034284346545c2d923ade2061e277a
CharleyCattac <ren.n.lol.l.sama@gmail.com>
2024-08-07 00:03:02 +0300

    added name

--- task_1_git/src/by/clevertec/task_1_git/Colour.java
+++ task_1_git/src/by/clevertec/task_1_git/Colour.java
@@ -1,7 +1,7 @@
 package by.clevertec.task_1_git;
 
 public abstract class Colour {
-    //protected String colourName;
+    protected String colourName;
     //protected String colourCode;
     // master comment
 }
